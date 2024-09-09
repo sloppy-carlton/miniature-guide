9a3146b6f28b2d58f3a251bf2f627bf693d38d05
sloppy-carlton <65428718+sloppy-carlton@users.noreply.github.com>
2024-09-09 10:50:31 -0500

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -4,7 +4,9 @@ echo "Now configuring your Arch system."
 ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
 hwclock
 pacman -Syu intel-ucode sof-firmware networkmanager wpa_supplicant nano man-db man-pages texinfo bluez bluez-utils reflector sudo alsa-utils pulseaudio pulseaudio-bluetooth pulseaudio-alsa pulseaudio-equalizer
-systemctl enable --now alsa-restore alsa-state
+systemctl enable --now bluetooth
+systemctl enable --now alsa-restore
+systemctl enable --now alsa-state
 amixer sset Master unmute
 amixer sset Speaker unmute
 amixer sset Headphone unmute

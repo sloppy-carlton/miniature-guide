eff8ad6e2ed6a3c38b2ee2a19fdb11f735c6f39d
sloppy-carlton <65428718+sloppy-carlton@users.noreply.github.com>
2024-09-09 20:02:51 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,9 +1,3 @@
-#!/bin/bash
- 
-git clone https://aur.archlinux.org/paru.git
-cd paru
-makepkg -i
-cd ~
 cat <<-ENDRC > ~/.bashrc
 	#
 	# ~/.bashrc
@@ -17,17 +11,3 @@ cat <<-ENDRC > ~/.bashrc
 	alias yeet='paru -R'
 	PS1='[\[\e[38;5;51;1;2m\]\u\[\e[0;1m\]@\[\e[38;5;51;2m\]\h\[\e[0m\]\w]\$ '
 	ENDRC
-if [[ "$(pacman -Q | grep -o 'lightdm')" == 'lightdm' ]];
-	then
-	paru -Sy arch-update lightdm-settings 
-else
-	paru -Sy arch-update
-fi
-rm -R ./paru
-if [[ "$(pacman-Q | grep -o 'enlightenment')" == 'enlightenment' ]];
-	then
-	paru -S econnman
-else
-	echo 'nothing else to do'
-fi
-echo 'Done'

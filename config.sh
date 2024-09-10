1df0f882a7b8cea519c4c5b2bddc6049dceebcaa
sloppy-carlton <65428718+sloppy-carlton@users.noreply.github.com>
2024-09-09 19:58:16 -0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
  
-su pmckenna -c cat <<-EOF
-	$(git clone https://aur.archlinux.org/paru.git)
-	$(cd paru)
-	$(makepkg -i)
-	$(cd ~)
-	$(cat <<-ENDRC > ~/.bashrc
-		#
-		# ~/.bashrc
-		#
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -i
+cd ~
+cat <<-ENDRC > ~/.bashrc
+	#
+	# ~/.bashrc
+ 	#
 		
-		# If not running interactively, don't do anything
-		[[ $- != *i* ]] && return
-		
-		alias ls='ls --color=auto'
-		alias grep='grep --color=auto'
-		alias yay='paru -Syupw && paru -c && paru -S --needed'
-		alias yeet='paru -R'
-		PS1='[\[\e[38;5;51;1;2m\]\u\[\e[0;1m\]@\[\e[38;5;51;2m\]\h\[\e[0m\]\w]\$ '
-	ENDRC)
-	$(if [[ "$(pacman -Q | grep -o 'lightdm')" == 'lightdm' ]];
+	# If not running interactively, don't do anything
+ 	[[ $- != *i* ]] && return	
+	alias ls='ls --color=auto'
+	alias grep='grep --color=auto'
+	alias yay='paru -Syupw && paru -c && paru -S --needed'
+	alias yeet='paru -R'
+	PS1='[\[\e[38;5;51;1;2m\]\u\[\e[0;1m\]@\[\e[38;5;51;2m\]\h\[\e[0m\]\w]\$ '
+	ENDRC
+if [[ "$(pacman -Q | grep -o 'lightdm')" == 'lightdm' ]];
 	then
 	paru -Sy arch-update lightdm-settings 
-	else
+else
 	paru -Sy arch-update
-	fi)
-	$(rm -R ./paru)
-	$(if [[ "$(pacman-Q | grep -o 'enlightenment')" == 'enlightenment' ]];
+fi
+rm -R ./paru
+if [[ "$(pacman-Q | grep -o 'enlightenment')" == 'enlightenment' ]];
 	then
 	paru -S econnman
-	else
+else
 	echo 'nothing else to do'
-	fi)
-EOF
-pacman -Rs rust
+fi
+echo 'Done'
